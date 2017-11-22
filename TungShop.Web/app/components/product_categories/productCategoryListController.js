ce3deafc990cf078431e0679a966c01117fade61
Tung.Nguyen <tung.nguyen@quant-edge.com>
2017-11-23 04:59:48 +0700

    them filter tim kiem

--- TungShop.Web/app/components/product_categories/productCategoryListController.js
+++ TungShop.Web/app/components/product_categories/productCategoryListController.js
@@ -8,22 +8,29 @@
         $scope.page = 0;
         $scope.pagesCount = 0;
         $scope.getProductCagories = getProductCagories;
+        $scope.keyword = '';
 
+        $scope.search = search;
+
+        function search() {
+            getProductCagories();
+        }
         function getProductCagories(page) {
             page = page || 0;
             var config = {
                 params: {
+                    keyword: $scope.keyword,
                     page: page,
                     pageSize: 20
                 }
             }
-            apiService.get("/api/productcategory/getall", config, function (result) {
+            apiService.get('/api/productcategory/getall', config, function (result) {
                 $scope.productCategories = result.data.Items;
                 $scope.page = result.data.Page;
                 $scope.pagesCount = result.data.TotalPages;
                 $scope.totalCount = result.data.TotalCount;
             }, function () {
-                console.log("Load productcategory failed.");
+                console.log('Load productcategory failed.');
             });
         }
 
