74a4e538301c2e8c03076aac5990abf52b606baa
Tung.Nguyen <tung.nguyen@quant-edge.com>
2017-11-23 13:41:05 +0700

    them moi productcategory

--- TungShop.Web/Api/ProductCategoryController.cs
+++ TungShop.Web/Api/ProductCategoryController.cs
@@ -8,6 +8,7 @@ using System.Web.Http;
 using TungShop.Model.Models;
 using TungShop.Service;
 using TungShop.Web.Infrastructure.Core;
+using TungShop.Web.Infrastructure.Extensions;
 using TungShop.Web.Models;
 
 namespace TungShop.Web.Api
@@ -21,6 +22,21 @@ namespace TungShop.Web.Api
             : base(errorService)
         {
             this._productCategoryService = productCategoryService;
+        }
+
+        [Route("getallparents")]
+        [HttpGet]
+        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                var model = _productCategoryService.GetAll();
+
+                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
+
+                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+                return response;
+            });
         }
 
         [Route("getall")]
@@ -45,6 +61,33 @@ namespace TungShop.Web.Api
                     TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize) // lam tron len
                 };
                 var response = request.CreateResponse(HttpStatusCode.OK, paginationSet);
+                return response;
+            });
+        }
+
+        [Route("create")]
+        [HttpPost]
+        public HttpResponseMessage Create(HttpRequestMessage request, ProductCategoryViewModel productCategoryVm)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var newProductCategory = new ProductCategory();
+                    newProductCategory.UpdateProductCategory(productCategoryVm);
+
+                    _productCategoryService.Add(newProductCategory);
+                    _productCategoryService.Save();
+
+                    var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(newProductCategory);
+                    response = request.CreateResponse(HttpStatusCode.Created, responseData);
+                }
+
                 return response;
             });
         }
